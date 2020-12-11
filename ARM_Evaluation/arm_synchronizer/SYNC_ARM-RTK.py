95e0db6b22311850a35eced9034450ca99e3e417
jan_kaderabek <xkadjan@gmail.com>
2020-12-11 17:02:42 +0100

    STAT:Synchronize static arm with RTK

--- ARM_Evaluation/arm_synchronizer/SYNC_ARM-RTK.py
+++ ARM_Evaluation/arm_synchronizer/SYNC_ARM-RTK.py
@@ -23,6 +23,8 @@ def configArgParser():
     parser.add('-of','--only_fix', type=lambda x: (str(x).lower() == 'true'), help='evaluate only fix samples')
     parser.add('-p','--prefix', type=str, help='evaluate only sliced time')
     parser.add('-r','--rate', type=int, help='MRA rate in Hz (for static measurement)')
+    parser.add('-s','--static', type=lambda x: (str(x).lower() == 'true'), help='evaluate only static measurement')
+    parser.add('-sp','--static_period', type=int, help='static measurement period in [s]')
     args = parser.parse()
     return args
 
@@ -40,7 +42,7 @@ if args.only_fix:
 else:
     measurement = 'Whole measurement'
 
-# pltr = plot.Plotter(args)
+pltr = plot.Plotter(args)
 
 # =============================================================================
 # ARM:
@@ -51,82 +53,81 @@ if args.new_preproccess:
     arm.get_bad_cicles()
     arm.drop_peaks()
     arm.create_static_reference()
-    
-    static = arm.static
-    
-   
-    
-    
-    # arm.filter_signal()
-    # arm.drop_bad_circles()
-    # arm.drop_zero_speed()
-    # arm.drop_zero_acc()
-    # arm.drop_limit_acc()
+    arm.filter_signal()
+    arm.drop_bad_circles()
+    arm.drop_zero_speed()
+    arm.drop_zero_acc()
+    arm.drop_limit_acc()
 
 # # =============================================================================
 # # RTK
 # # =============================================================================
-# if args.new_preproccess:
-#     rtk = rtk_prs.RtkParser(args)
-#     rtk.parse_slices()
-#     rtk.drop_points_wo_arm(arm.arm_20hz)
-#     rtk.drop_points_wo_rtk(arm.arm_20hz)
-#     rtk_list = rtk.concate_arm_and_rtks()
+if args.new_preproccess:
+    rtk = rtk_prs.RtkParser(args)
+    rtk.parse_slices()
+    static = True
+    if args.static:
+        rtk.drop_points_wo_arm(arm.static)
+        rtk.drop_points_wo_rtk(arm.static)
+    else:
+        rtk.drop_points_wo_arm(arm.arm_20hz)
+        rtk.drop_points_wo_rtk(arm.arm_20hz)
+    rtk_list = rtk.concate_arm_and_rtks()
 
-#     pltr.plot_rtk(rtk.novatel,args.rtk_names[0],"g")
-#     pltr.plot_rtk(rtk.tersus,args.rtk_names[1],"y")
-#     pltr.plot_rtk(rtk.ashtech,args.rtk_names[2],"b")
-#     pltr.plot_rtk(rtk.ublox,args.rtk_names[3],"m")
+    # pltr.plot_rtk(rtk.novatel,args.rtk_names[0],"g")
+    # pltr.plot_rtk(rtk.tersus,args.rtk_names[1],"y")
+    # pltr.plot_rtk(rtk.ashtech,args.rtk_names[2],"b")
+    # pltr.plot_rtk(rtk.ublox,args.rtk_names[3],"m")
 
-# # =============================================================================
-# # EVL
-# # =============================================================================
-# evl = rtk_evl.Evaluator()
-# if not args.new_preproccess:
-#     evl.csv_load(args.result_dir)
-#     rtk_list = evl.csv_load(checkFolder(args.result_dir))
-# evl.get_deviations(rtk_list)
-# if args.only_fix:
-#     evl.filter_fix()
-# # evl.filter_sigma()
-# # evl.abs_acc()
-# evl.get_make_boxes()
-# # evl.adjust_status()
-# evl.get_results(args.only_fix)
+# =============================================================================
+# EVL
+# =============================================================================
+evl = rtk_evl.Evaluator()
+if not args.new_preproccess:
+    evl.csv_load(args.result_dir)
+    rtk_list = evl.csv_load(checkFolder(args.result_dir))
+evl.get_deviations(rtk_list)
+if args.only_fix:
+    evl.filter_fix()
+# evl.filter_sigma()
+# evl.abs_acc()
+evl.get_make_boxes()
+# evl.adjust_status()
+evl.get_results(args.only_fix)
 
-# evl.csv_print(checkFolder(args.result_dir),args.new_preproccess)
+evl.csv_print(checkFolder(args.result_dir),args.new_preproccess)
 
-# evl.get_correlation()
-# evl.get_spearman()
-# evl.get_median()
+evl.get_correlation()
+evl.get_spearman()
+evl.get_median()
 
-# # =============================================================================
-# # PLOTTING
-# # =============================================================================
+# =============================================================================
+# PLOTTING
+# =============================================================================
 
-# # Print deviations (The density of occurrence of horizontal deviations from the reference trajectory of the MRA in the latitude and longitude directions)
-# pltr.plot_hist(evl.novatel,args.rtk_names[0],evl.results_novatel.iloc[0],measurement)
-# pltr.plot_hist(evl.tersus,args.rtk_names[1],evl.results_tersus.iloc[0],measurement)
-# pltr.plot_hist(evl.ashtech,args.rtk_names[2],evl.results_ashtech.iloc[0],measurement)
-# pltr.plot_hist(evl.ublox,args.rtk_names[3],evl.results_ublox.iloc[0],measurement)
+# Print deviations (The density of occurrence of horizontal deviations from the reference trajectory of the MRA in the latitude and longitude directions)
+pltr.plot_hist(evl.novatel,args.rtk_names[0],evl.results_novatel.iloc[0],measurement)
+pltr.plot_hist(evl.tersus,args.rtk_names[1],evl.results_tersus.iloc[0],measurement)
+pltr.plot_hist(evl.ashtech,args.rtk_names[2],evl.results_ashtech.iloc[0],measurement)
+pltr.plot_hist(evl.ublox,args.rtk_names[3],evl.results_ublox.iloc[0],measurement)
 
-# # Print density (The density of deviations)
-# pltr.plot_hist_dev(evl.novatel.deviation,args.rtk_names[0],evl.results_novatel.iloc[0],measurement)
-# pltr.plot_hist_dev(evl.tersus.deviation,args.rtk_names[1],evl.results_tersus.iloc[0],measurement)
-# pltr.plot_hist_dev(evl.ashtech.deviation,args.rtk_names[2],evl.results_ashtech.iloc[0],measurement)
-# pltr.plot_hist_dev(evl.ublox.deviation,args.rtk_names[3],evl.results_ublox.iloc[0],measurement)
+# Print density (The density of deviations)
+pltr.plot_hist_dev(evl.novatel.deviation,args.rtk_names[0],evl.results_novatel.iloc[0],measurement)
+pltr.plot_hist_dev(evl.tersus.deviation,args.rtk_names[1],evl.results_tersus.iloc[0],measurement)
+pltr.plot_hist_dev(evl.ashtech.deviation,args.rtk_names[2],evl.results_ashtech.iloc[0],measurement)
+pltr.plot_hist_dev(evl.ublox.deviation,args.rtk_names[3],evl.results_ublox.iloc[0],measurement)
 
-# # Print deviatiton by status (The distribution of deviations during individual states)
-# pltr.plot_boxplot(evl.novatel,args.rtk_names[0],'status',measurement)
-# pltr.plot_boxplot(evl.tersus,args.rtk_names[1],'status',measurement)
-# pltr.plot_boxplot(evl.ashtech,args.rtk_names[2],'status',measurement)
-# pltr.plot_boxplot(evl.ublox,args.rtk_names[3],'status',measurement)
+# Print deviatiton by status (The distribution of deviations during individual states)
+pltr.plot_boxplot(evl.novatel,args.rtk_names[0],'status',measurement)
+pltr.plot_boxplot(evl.tersus,args.rtk_names[1],'status',measurement)
+pltr.plot_boxplot(evl.ashtech,args.rtk_names[2],'status',measurement)
+pltr.plot_boxplot(evl.ublox,args.rtk_names[3],'status',measurement)
 
-# # Print deviatiton by phase (The distribution of deviations during individual phases of scenario)
-# pltr.plot_boxplot(evl.novatel_by_acc,args.rtk_names[0],'phase',measurement)
-# pltr.plot_boxplot(evl.tersus_by_acc,args.rtk_names[1],'phase',measurement)
-# pltr.plot_boxplot(evl.ashtech_by_acc,args.rtk_names[2],'phase',measurement)
-# pltr.plot_boxplot(evl.ublox_by_acc,args.rtk_names[3],'phase',measurement)
+# Print deviatiton by phase (The distribution of deviations during individual phases of scenario)
+pltr.plot_boxplot(evl.novatel_by_acc,args.rtk_names[0],'phase',measurement)
+pltr.plot_boxplot(evl.tersus_by_acc,args.rtk_names[1],'phase',measurement)
+pltr.plot_boxplot(evl.ashtech_by_acc,args.rtk_names[2],'phase',measurement)
+pltr.plot_boxplot(evl.ublox_by_acc,args.rtk_names[3],'phase',measurement)
 
 # # # =============================================================================
 # # # import numpy as np
